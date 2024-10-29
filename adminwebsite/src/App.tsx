ca11631cc7d97a7b2826e75d05e05ecb9bcc22b4
subhadeepsonu <subhadeepthandaka@gmail.com>
2024-10-30 00:01:04 +0530

    intial commit

---
+++ adminwebsite/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='text-2xl font-bold h-screen w-full flex justify-center items-center'>
+      evf
+    </div>
+  )
+}
+
+export default App
---
+++ userwebsite/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='h-screen w-full flex justify-center items-center '>
+      svsfd
+    </div>
+  )
+}
+
+export default App
