044047153c804ffb0a6e7bf59891434d7b22ef84
subhadeepsonu <subhadeepthandaka@gmail.com>
2024-11-03 00:09:28 +0530

    intail setup of master

--- master/index.ts
+++ master/index.ts
@@ -1,5 +1,6 @@
 import express from "express"
 const app = express()
+app.use(express.json())
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
--- slave/index.ts
+++ slave/index.ts
@@ -1,6 +1,15 @@
 import express from "express"
 const app = express()
-
+import redis from "redis"
+async function slave() {
+    while (true) {
+        const client = redis.createClient()
+        await client.connect()
+        const submissions = client.brPop("submissions", 0)
+        console.log(submissions)
+    }
+}
+slave()
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
 }
